23320200ccc6de448221a96641ef0ddead7dd974
Jinnat Ara Khatun <157870456+Jinnat36@users.noreply.github.com>
2024-07-31 10:40:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var typed = new Typed(".text",{
     backDelay : 1000,
     loop : true
 });
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
